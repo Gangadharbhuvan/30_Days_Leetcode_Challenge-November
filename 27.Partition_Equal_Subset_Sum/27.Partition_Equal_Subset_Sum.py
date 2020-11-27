eb91ffa723b1d17f6d7f0bb7ba825f6e77487c38
Gangadhar DN <44730031+Gangadharbhuvan@users.noreply.github.com>
2020-11-27 18:50:10 +0530

    Added Day-27 Solution

---
+++ 27.Partition_Equal_Subset_Sum/27.Partition_Equal_Subset_Sum.py
@@ -0,0 +1,28 @@
+'''
+	Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+
+'''
+
+class Solution:
+    def canPartition(self, A):
+        a = reduce(lambda a, num: a|(a<<num), A, 1)
+        return sum(A)%2 == 0 and a & (1 << (sum(A)//2)) != 0
